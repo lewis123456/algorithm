69153cbd896eab24b869faaecf4723f735e32468
lewis123456 <2328811532@qq.com>
2018-08-03 18:12:44 +0800

    TH0001

--- src/TH0001.java
+++ src/TH0001.java
@@ -1,20 +1,37 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class TH0001 {
     public static void main(String[] args) {
-
-        ArrayList<Cat> arrayList = new ArrayList<>();
-        Random random = new Random();
+        List<Cat> arrayList = new ArrayList<>();
+        Random random = new Random(System.currentTimeMillis());
         for (int i=0; i<100; ++i) {
             Cat cat = new TH0001().new Cat();
-            cat.setIndex(i);
+            cat.setIndex(random.nextInt(100));
             cat.setColor(MYCOLOR.values()[random.nextInt(11)]);
             arrayList.add(cat);
         }
-
+        System.out.println(arrayList);
         //将arraryList排序，优先保证color的顺序，其次保证index的顺序
-
+        //1. 插入排序
+        for (int i=1; i<100; ++i) {
+            Cat now = arrayList.get(i);
+            for (int j=i-1; j>=0; --j) {
+                if (now.getColor().ordinal() < arrayList.get(j).getColor().ordinal() ||
+                        (now.getColor().ordinal() == arrayList.get(j).getColor().ordinal() && 
+                                now.getIndex() < arrayList.get(j).getIndex())) {
+                    arrayList.set(j+1, arrayList.get(j));
+                } else {
+                    arrayList.set(j+1, now);
+                    break;
+                }
+                if (0 == j) {
+                    arrayList.set(0, now);
+                }
+            }
+        }
+        System.out.println(arrayList);
     }
 
     private class Cat {
@@ -35,6 +52,11 @@ public class TH0001 {
 
         public void setColor(MYCOLOR color) {
             this.color = color;
+        }
+
+        @Override
+        public String toString() {
+            return "[index=" + index + ", color=" + color + "]";
         }
     }
 
